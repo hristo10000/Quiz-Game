08de55b3992b8453c1276374039a7cef10506147
ViktorMilanov <101833400+ViktorMilanov@users.noreply.github.com>
2022-07-19 17:50:56 +0300

    Routes are added

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import LogIn from "./components/";
+import SignUp from "./routes/invoices";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="expenses" element={<LogIn />} />
+      <Route path="invoices" element={<SignUp />} />
+    </Routes>
   </BrowserRouter>,
 );
 
