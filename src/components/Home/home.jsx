e70a4cd2e42ca7a78ff213d28d42d17550b2b94b
Hristo Ivanov <103217093+hristo10000@users.noreply.github.com>
2022-07-19 15:16:03 +0300

    fixed the url issue
    the url is now 'http://localhost:3000/' instead of 'http://localhost:3000/hristo10000/Quest Game'

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -4,11 +4,10 @@ import logo from '../../images/logo.png';
 function HomePage() {
   return (
     <>
-      <img src={logo} className="App-logo" alt="logo" height={500} width={500} />
+      <img src={logo} className="App-logo" alt="logo" height={200} width={300} />
       <h2>Welcome to Quiz Quest</h2>
       <h5>The quest to become the best quizer.</h5>
     </>
-
   );
 }
 
