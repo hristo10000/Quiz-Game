73197c7ee40e7662e2a9e32e1ca314dbc1ee432f
ViktorMilanov <101833400+ViktorMilanov@users.noreply.github.com>
2022-07-20 13:30:08 +0300

    Update SignUpForm.jsx

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 
 function SignUpForm() {
+  const [setformValue] = React.useState({
+    username: '',
+    password: '',
+  });
+
+  const handleChangeOnUsername = (event) => {
+    setformValue.username = event.target.value;
+  };
+
+  const handleChangeOnPassword = (event) => {
+    setformValue.password = event.target.value;
+  };
+
+  // handleSubmit = (event) => {
+  //   event.preventDefault();
+  // };
+
   return (
     <>
       <h2>Sign Up</h2>
       <form>
-        <input label="username" htmlFor="username" required="true" />
-        <input label="password" htmlFor="password" required="true" />
+        <input label="username" htmlFor="username" required onChange={handleChangeOnUsername} />
+        <input label="password" htmlFor="password" required onChange={handleChangeOnPassword} />
         <input type="submit" />
       </form>
     </>
