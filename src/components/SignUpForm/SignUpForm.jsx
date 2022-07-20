879b063741a7f927d881e509636641034d8d2c22
ViktorMilanov <101833400+ViktorMilanov@users.noreply.github.com>
2022-07-20 10:49:12 +0300

    Added header to Sign up form

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 function SignUpForm() {
   return (
-    <form>
-      <input label="username" htmlFor="username" required="true" />
-      <input label="password" htmlFor="password" required="true" />
-      <input type="submit" />
-    </form>
+    <>
+      <h2>Sign Up</h2>
+      <form>
+        <input label="username" htmlFor="username" required="true" />
+        <input label="password" htmlFor="password" required="true" />
+        <input type="submit" />
+      </form>
+    </>
   );
 }
 
