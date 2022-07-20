b1ae420e97cd816581159c487b9404e71a76d695
ViktorMilanov <101833400+ViktorMilanov@users.noreply.github.com>
2022-07-20 10:50:27 +0300

    Update LogInForm.jsx

--- src/components/LogInForm/LogInForm.jsx
+++ src/components/LogInForm/LogInForm.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 function LogInForm() {
   return (
-    <form>
-      <input label="username" htmlFor="username" required="true" />
-      <input label="password" htmlFor="password" required="true" />
-      <input type="submit" />
-    </form>
+    <>
+      <h2>Log In</h2>
+      <form>
+        <input label="username" htmlFor="username" required="true" />
+        <input label="password" htmlFor="password" required="true" />
+        <input type="submit" />
+      </form>
+    </>
   );
 }
 
