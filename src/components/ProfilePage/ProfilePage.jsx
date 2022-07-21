9efe211bde2c2082925af2db49aca5f3b0a0066a
ViktorMilanov <101833400+ViktorMilanov@users.noreply.github.com>
2022-07-21 15:04:43 +0300

    Folder name fix

--- src/components/ProfilePage/ProfilePage.jsx
+++ src/components/ProfilePage/ProfilePage.jsx
@@ -3,8 +3,6 @@ import React from 'react';
 function ProfilePage() {
   const token = localStorage.getItem('token');
   console.log(token);
-  const UserInfo = localStorage.getItem('token')[1];
-  console.log(UserInfo);
   return (<p>a</p>);
 }
 
