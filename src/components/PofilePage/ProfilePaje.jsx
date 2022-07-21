92fa72b155526cd1e10f73e2fcce1b895310d5c3
Hristo Ivanov <103217093+hristo10000@users.noreply.github.com>
2022-07-21 11:22:17 +0300

    creaed profile page
    Its empty

---
+++ src/components/PofilePage/ProfilePaje.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function ProfilePage(userInfo) {
+  console.log(userInfo);
+  return (<p>a</p>);
+}
+
+export default ProfilePage;
--- src/components/Profile.jsx
+++
@@ -1,4 +0,0 @@
-function Profile() {
-  return console.log(this.props.location.state.token);
-}
-export default Profile;
