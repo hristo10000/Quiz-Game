f9deb8d5a33b0fd6c6f25e984313297dbf1538fd
Hristo Ivanov <103217093+hristo10000@users.noreply.github.com>
2022-07-25 10:46:55 +0300

    removed useless css and added css

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../images/logo.png';
 import Button from '../Button/Button';
+import '../../App.css';
 
 function LandingPage() {
   return (
