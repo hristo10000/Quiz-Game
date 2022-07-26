f9443af94820acf9cf66f83ecba90c7ca0cdf01b
Hristo Ivanov <103217093+hristo10000@users.noreply.github.com>
2022-07-26 16:17:57 +0300

    user works with catche

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,16 +1,17 @@
 import React, { useEffect } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 import instance from '../../utils/Requests';
-import AcceptInvite from '../Game/AcceptPage';
+import CustomButton from '../Button/Button';
+import Invitation from '../Game/Invitation';
+import cache from '../../utils/cache';
 
 function Home() {
   const [username, setUsername] = React.useState('');
-
-  const [invitation, setInvitation] = React.useState({});
-  setInvitation(undefined);
-
-  const [user, setUser] = React.useState({});
+  const [invitation, setInvitation] = React.useState('');
+  const me = cache.get('me');
 
   const navigate = useNavigate();
+
   useEffect(() => {
     const token = localStorage.getItem('token');
     instance.defaults.headers.common.Authorization = `Token ${token}`;
@@ -21,13 +22,10 @@ function Home() {
     };
 
     ws.onmessage = (e) => {
-      const { type, data } = JSON.parse(e.data);
-      console.log(type, data);
-      if (data.invited === user.username) {
+      const { data } = JSON.parse(e.data);
+      if (data.invited === me.username) {
+        setInvitation(data);
         navigate('/accept');
-      }
-      if (data.invited_by.username === user.username) {
-        navigate('/game', { gameToken: data.channel });
       }
     };
 
@@ -38,38 +36,35 @@ function Home() {
     ws.onclose = (e) => {
       console.log('CLOSED', e);
     };
-  });
-
-  useEffect(() => {
-    instance.get('/api/players/me/').then(({ data }) => setUser(data));
   }, []);
 
-  // const sendInvite = () => {
-  //   if (username !== user?.username) {
-  //     instance.post('/api/games/', { username });
-  //   } else {
-  //     navigate('/home');
-  //   }
-  // };
+  const sendInvite = () => {
+    if (username !== me?.username) {
+      instance.post('/api/games/', { username });
+    } else {
+      navigate('/home');
+    }
+  };
   const handleChange = (event) => {
     event.preventDefault();
     setUsername(event.target.value);
   };
 
-  const sendInvite = () => {
-    instance.post('/api/games/', { username });
-  };
-
   return (
     <>
-      {/* <Header /> */}
-      {
-        invitation && <AcceptInvite invited_by />
-      }
-      <div className="invite-search">
-        <input onChange={handleChange} type="text" placeholder="enter username of another player" />
+      <h1>{me?.username}</h1>
+
+      <Link to="/logout"><CustomButton text="Log Out" /></Link>
+
+      <div className="InviteSearch">
+
+        <input onChange={handleChange} type="text" placeholder="enter oponent's username" />
+
         <input onClick={sendInvite} type="submit" value="Invite" />
+
       </div>
+
+      {invitation && <Invitation invitedBy={invitation.invited_by.username} /> }
     </>
   );
 }
