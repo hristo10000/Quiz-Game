0f628920bd54eb1fe970a6a5366246505cdccfc9
ViktorMilanov <101833400+ViktorMilanov@users.noreply.github.com>
2022-07-28 14:23:35 +0300

    Loser info is printed on end game also

--- src/components/EndGame.jsx
+++ src/components/EndGame.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
+import cache from '../utils/cache';
 
 function EndGame() {
+  const winner = cache.get('winner');
+  const loser = cache.get('loser');
   return (
-    <h1>
-      The Winner is
-      {' '}
-      {localStorage.getItem('winner')}
-      {' '}
-      with score:
-      {' '}
-      {localStorage.getItem('score')}
-    </h1>
+    <>
+      <h1>
+        The Winner is
+        {' '}
+        {winner?.username}
+        {' '}
+        with score:
+        {' '}
+        {winner?.score}
+      </h1>
+      <h1>
+        The Loser is
+        {' '}
+        {loser?.username}
+        {' '}
+        with score:
+        {' '}
+        {loser?.score}
+      </h1>
+
+    </>
   );
 }
 
